ad56abbb12d9d0320c1f4eceb1b2b6edc41a5c44
Idris Bowman <34751375+V00D00-child@users.noreply.github.com>
2023-06-03 23:16:07 -0400

    copy update

--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -158,7 +158,7 @@ const Index = () => {
           content={{
             title: 'What',
             description:
-              `ERC-4337 Relayer is a tool that makes it easy for developers and MetaMask wallet users to use ERC-4337 without dealing with its complexity.`,
+              `ERC-4337 Relayer is a snap that makes it easy for developers and MetaMask wallet users to use ERC-4337 without dealing with its complexity.`,
           }}
           fullWidth
         />

598c4a60f705e6e167d21234aa0a9ac7ccb3db75
Idris Bowman <34751375+V00D00-child@users.noreply.github.com>
2023-06-11 19:17:34 -0400

    add withdraw form

--- packages/site/src/pages/index.tsx
+++ packages/site/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useState } from 'react';
 import styled from 'styled-components';
 import { MetamaskActions, MetaMaskContext } from '../hooks';
 import {
